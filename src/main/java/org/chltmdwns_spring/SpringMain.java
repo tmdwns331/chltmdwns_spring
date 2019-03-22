a1e96b25e8af0f0020d663ff82c7fdce8f0693a2
tmdwns <https://github.com/tmdwns331/tmdwns-jdbc.git>
2019-03-22 18:21:12 +0900

    소스 추가

---
+++ src/main/java/org/chltmdwns_spring/SpringMain.java
@@ -0,0 +1,22 @@
+package org.chltmdwns_spring;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringMain {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("chap02.xml");
+		Greeter g1 = ctx.getBean("greeter", Greeter.class);
+		System.out.println(g1.greet());
+
+		Greeter g2 = ctx.getBean("greeter", Greeter.class);
+		System.out.println(g2.greet());
+
+		Greeter g3 = ctx.getBean("greeter", Greeter.class);
+		System.out.println(g3.greet());
+
+	}
+
+}
